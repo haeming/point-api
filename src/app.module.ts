ba63bc0f8bb13e3c6e49d5ae362b9cb419998012
haeming <goalsgoals0417@gmail.com>
2025-11-07 16:11:16 +0900

    feat: 서버 정상 가동 확인용 AppController 및 AppService 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import {MemoryDb} from "./common/data/memory.db";
+import {AppController} from "./app.controller";
+import {AppService} from "./app.service";
 
 @Module({
     imports: [],
-    controllers: [],
-    providers: [MemoryDb],
+    controllers: [AppController],
+    providers: [AppService, MemoryDb],
 })
 export class AppModule {}
