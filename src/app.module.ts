8cbd86d21d1e0867ead4e202fe4400f01cceff5f
haeming <goalsgoals0417@gmail.com>
2025-11-08 21:52:45 +0900

    feat: JWT 인증 로직 및 사용자 정보 API 구현

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,12 @@ import {Module} from '@nestjs/common';
 import {AppController} from "./app.controller";
 import {AppService} from "./app.service";
 import {CommonModule} from "./common/common.module";
+import {AuthModule} from "./auth/auth.module";
+import {UserController} from "./users/user.controller";
 
 @Module({
-    imports: [CommonModule],
-    controllers: [AppController],
+    imports: [CommonModule, AuthModule],
+    controllers: [AppController, UserController],
     providers: [AppService],
 })
 export class AppModule {}
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,19 +1,19 @@
 import {Module} from "@nestjs/common";
 import {JwtModule} from "@nestjs/jwt";
-import * as process from "node:process";
 import {AuthController} from "./controller/auth.controller";
 import {AuthService} from "./service/auth.service";
 import {JwtStrategy} from "./jwt.strategy";
+import {CommonModule} from "../common/common.module";
 
 @Module({
     imports: [
         JwtModule.register({
-            secret: process.env.JWT_SECRET || "testsecret",
+            secret: "rhkwpdyddlqslekrltdpdksdhffkrkfRkqhkrmsidwjrtmqslek",
             signOptions: {expiresIn: "1h"},
         }),
     ],
     controllers: [AuthController],
-    providers: [AuthService, JwtStrategy],
+    providers: [AuthService, JwtStrategy, CommonModule],
     exports: [AuthService]
 })
 export class AuthModule {}
