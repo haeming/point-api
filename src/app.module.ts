5e2bdc42182acf0b4ca1513b890de74da406a1af
haeming <goalsgoals0417@gmail.com>
2025-11-08 22:22:20 +0900

    Revert "feat: JWT 인증 로직 및 사용자 정보 API 구현 (#4)"
    This reverts commit 122f57f9ef51ba8d525bce78732f455bbcfab757.

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,10 @@ import {Module} from '@nestjs/common';
 import {AppController} from "./app.controller";
 import {AppService} from "./app.service";
 import {CommonModule} from "./common/common.module";
-import {AuthModule} from "./auth/auth.module";
-import {UserController} from "./users/user.controller";
 
 @Module({
-    imports: [CommonModule, AuthModule],
-    controllers: [AppController, UserController],
+    imports: [CommonModule],
+    controllers: [AppController],
     providers: [AppService],
 })
 export class AppModule {}
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,19 +1,19 @@
 import {Module} from "@nestjs/common";
 import {JwtModule} from "@nestjs/jwt";
+import * as process from "node:process";
 import {AuthController} from "./controller/auth.controller";
 import {AuthService} from "./service/auth.service";
 import {JwtStrategy} from "./jwt.strategy";
-import {CommonModule} from "../common/common.module";
 
 @Module({
     imports: [
         JwtModule.register({
-            secret: "rhkwpdyddlqslekrltdpdksdhffkrkfRkqhkrmsidwjrtmqslek",
+            secret: process.env.JWT_SECRET || "testsecret",
             signOptions: {expiresIn: "1h"},
         }),
     ],
     controllers: [AuthController],
-    providers: [AuthService, JwtStrategy, CommonModule],
+    providers: [AuthService, JwtStrategy],
     exports: [AuthService]
 })
 export class AuthModule {}
